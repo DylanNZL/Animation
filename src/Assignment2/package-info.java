f9937506bdf5be1afd1b2b0c6ab559fe614e0d93
Dylan Cross <dylnnzl@gmail.com>
2016-09-17 17:38:09 +1200

    REFACTOR

--- src/Assignment2/package-info.java
+++ src/Assignment2/package-info.java
@@ -3,9 +3,6 @@
  * Used IntelliJ (latest) & JDK 1.8.040
  * Assignment 2 for 159.235 Graphical Programming
  *
- */
-
-/**
  * Write a program called Animation that provides the following features:
  *  • Can load the provided sprite sheet and is able to draw the individual sprites contained on it. Note that each of the sprites is 256x256 pixels with no padding between them and there is one animation sequence per row.
  *  • The program initially displays the sprite in the top left (state underground), centred at the bottom of the frame.
@@ -17,5 +14,4 @@
  *      ◦ Space: play the attack animation (row 5).
  *  • When no action is active and the skeleton is not in state underground or dead, play the idle animation (row 2).
  *  • While the skeleton is underground or dead, the only possible action is to appear.
- *
  */
